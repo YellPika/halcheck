13294961effeefd72eee0f82bac066e5e3944a6a
YellPika <yellpika@gmail.com>
2024-01-23 12:48:25 -0500

    group.hpp: add if_ and while_

--- include/halcheck/gen/container.hpp
+++ include/halcheck/gen/container.hpp
@@ -25,16 +25,16 @@ template<
     HALCHECK_REQUIRE(lib::is_insertable<T>()),
     HALCHECK_REQUIRE(lib::is_invocable_r<lib::range_value_t<T>, F>())>
 T container(F gen) {
-  auto _ = gen::group();
-  auto size = gen::size();
-
   T output;
   auto it = lib::end(output);
-  while (gen::next(1, size--)) {
-    auto _ = gen::group();
+  auto size = gen::size();
+  gen::while_([&] {
+    if (!gen::next(1, size--))
+      return false;
     if (!gen::shrink())
       it = std::next(output.insert(it, lib::invoke(gen)));
-  }
+    return true;
+  });
   return output;
 }
 
@@ -50,14 +50,16 @@ template<
     HALCHECK_REQUIRE(lib::is_insertable<T>()),
     HALCHECK_REQUIRE(lib::is_invocable_r<lib::range_value_t<T>, F>())>
 T container(std::size_t size, F gen) {
-  auto _ = gen::group();
-
   T output;
   auto it = lib::end(output);
-  while (size-- > 0) {
-    auto _ = gen::group();
+  gen::while_([&] {
+    if (size == 0)
+      return false;
+
+    --size;
     it = std::next(output.insert(it, lib::invoke(gen)));
-  }
+    return true;
+  });
   return output;
 }
 
--- include/halcheck/gen/group.hpp
+++ include/halcheck/gen/group.hpp
@@ -4,6 +4,7 @@
 #include <halcheck/lib/effect.hpp>
 #include <halcheck/lib/functional.hpp>
 #include <halcheck/lib/scope.hpp>
+#include <halcheck/lib/type_traits.hpp>
 
 namespace halcheck { namespace gen {
 
@@ -43,6 +44,29 @@ public:
     return effect::handle(invoke{this, std::move(func)});
   }
 } group;
+
+template<
+    typename F1,
+    typename F2 = void (*)(),
+    HALCHECK_REQUIRE(lib::is_invocable<F1>()),
+    HALCHECK_REQUIRE(lib::is_invocable<F2>())>
+void if_(
+    bool cond, F1 f1, F2 f2 = [] {}) {
+  if (cond) {
+    gen::group(f1);
+    gen::group();
+  } else {
+    gen::group();
+    gen::group(f2);
+  }
+}
+
+template<typename F, HALCHECK_REQUIRE(lib::is_invocable_r<bool, F>())>
+void while_(F func) {
+  auto _ = gen::group();
+  while (gen::group(func))
+    ;
+}
 
 }} // namespace halcheck::gen
 
