880566ff5a6a2a46a4bf1b01d6ba001b816eba4d
YellPika <yellpika@gmail.com>
2024-04-24 11:56:05 -0400

    discard.hpp: add backtrack

--- include/halcheck/gen/discard.hpp
+++ include/halcheck/gen/discard.hpp
@@ -4,6 +4,7 @@
 #include <halcheck/gen/group.hpp>
 #include <halcheck/gen/next.hpp>
 #include <halcheck/lib/effect.hpp>
+#include <halcheck/lib/optional.hpp>
 #include <halcheck/lib/raise.hpp>
 
 namespace halcheck { namespace gen {
@@ -21,7 +22,7 @@ extern lib::effect<void> succeed;
 
 /// @brief Executes a function. If the function calls discard, then it is
 ///        retried, up to a certain number of times.
-/// @tparam T The type of function to call.
+/// @tparam F The type of function to call.
 /// @param max The maximum number of tries.
 /// @param func The function to call.
 /// @return The result of func.
@@ -50,12 +51,38 @@ lib::invoke_result_t<F> retry(std::intmax_t max, F func) {
 
 /// @brief Executes a function. If the function calls discard, then it is
 ///        retried, up to 100 times.
-/// @tparam T The type of function to call.
+/// @tparam F The type of function to call.
 /// @param func The function to call.
 /// @return The result of func.
 template<typename F, HALCHECK_REQUIRE(lib::is_invocable<F>())>
 lib::invoke_result_t<F> retry(F func) {
   return gen::retry(100, std::move(func));
+}
+
+/// @brief Executes a function. If the function calls discard before calling
+/// next, then lib::nullopt is returned.
+/// @tparam F The type of function to invoke.
+/// @param func The function to invoke.
+/// @return The return value of func, or lib::nullopt.
+template<typename F, HALCHECK_REQUIRE(lib::is_invocable<F>())>
+lib::optional<lib::invoke_result_t<F>> backtrack(F func) {
+  return gen::group([&]() -> lib::optional<lib::invoke_result_t<F>> {
+    struct discard {};
+    bool ok = true;
+    try {
+      auto _0 = gen::next.handle([&](std::uintmax_t w0, std::uintmax_t w1) {
+        ok = false;
+        return gen::next(w0, w1);
+      });
+      auto _1 = gen::discard.handle([&] { return lib::raise(discard()); });
+      return lib::invoke(std::move(func));
+    } catch (const discard &) {
+      if (ok)
+        return lib::nullopt;
+      else
+        gen::discard();
+    }
+  });
 }
 
 }} // namespace halcheck::gen
