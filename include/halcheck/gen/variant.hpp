a9209ec2e55eb987b30b781fe156af1f652abe20
YellPika <yellpika@gmail.com>
2024-04-26 11:29:59 -0400

    Revert "variant.hpp: backtrack when possible"
    This reverts commit d38b95ce000910d7846db6a82519c66118891a6d.

--- include/halcheck/gen/variant.hpp
+++ include/halcheck/gen/variant.hpp
@@ -13,6 +13,19 @@
 
 namespace halcheck { namespace gen {
 
+namespace detail {
+template<typename... Fs>
+struct variant_visitor {
+  template<typename T>
+  lib::variant<lib::invoke_result_t<Fs>...> operator()(T i) {
+    static constexpr auto I = decltype(i)::value;
+    return lib::variant<lib::invoke_result_t<Fs>...>(lib::in_place_index_t<I>(), lib::invoke(std::get<I>(tuple)));
+  }
+  variant_visitor(Fs... gens) : tuple(std::move(gens)...) {}
+  std::tuple<Fs...> tuple;
+};
+} // namespace detail
+
 /// @brief Generates a random std::integral_constant from the given set of
 /// indices.
 /// @tparam Ints The set of indices to pick from.
@@ -23,44 +36,16 @@ lib::variant<std::integral_constant<std::size_t, Ints>...> index(lib::index_sequ
   return gen::element({T(std::integral_constant<std::size_t, Ints>())...});
 }
 
-namespace detail {
-
-template<typename Seq, typename... Fs>
-struct variant_visitor {
-  template<std::size_t I>
-  lib::variant<lib::invoke_result_t<Fs>...> operator()(std::integral_constant<std::size_t, I>) {
-    if (auto result = gen::backtrack(std::get<I>(tuple)))
-      return lib::variant<lib::invoke_result_t<Fs>...>(lib::in_place_index_t<I>(), *result);
-    else
-      return variant_visitor<lib::remove<I, Seq>, Fs...>{std::move(tuple)}.apply();
-  }
-
-  template<std::size_t N = 1, HALCHECK_REQUIRE(lib::sequence_size(Seq()) > N)>
-  lib::variant<lib::invoke_result_t<Fs>...> apply() {
-    return lib::visit(*this, gen::index(Seq()));
-  }
-
-  template<std::size_t N = 1, HALCHECK_REQUIRE(lib::sequence_size(Seq()) == N)>
-  lib::variant<lib::invoke_result_t<Fs>...> apply() {
-    return lib::variant<lib::invoke_result_t<Fs>...>(lib::in_place_index_t<0>(), std::get<0>(tuple)());
-  }
-
-  std::tuple<Fs...> tuple;
-};
-
-} // namespace detail
-
 /// @brief Constructs a random variant.
 /// @tparam Fs The generator function types for each variant element type.
-/// @param gens The generators for each variant element type. If any of these
-/// generators discard without calling next, then a different generator is
-/// attempted.
+/// @param gens The generators for each variant element type.
 /// @return A variant whose value is drawn from a randomly selected element of
 /// gens.
 template<typename... Fs, HALCHECK_REQUIRE(lib::conjunction<lib::is_invocable<Fs>...>())>
 lib::variant<lib::invoke_result_t<Fs>...> variant(Fs... gens) {
   auto _ = gen::group();
-  return detail::variant_visitor<lib::make_index_sequence<sizeof...(Fs)>, Fs...>{{std::move(gens)...}}.apply();
+  return lib::visit(
+      detail::variant_visitor<Fs...>(std::move(gens)...), gen::index(lib::make_index_sequence<sizeof...(Fs)>()));
 }
 
 }} // namespace halcheck::gen
--- include/halcheck/lib/utility.hpp
+++ include/halcheck/lib/utility.hpp
@@ -39,37 +39,6 @@ template<std::size_t N>
 using make_index_sequence = make_integer_sequence<std::size_t, N>;
 #endif
 
-namespace detail {
-template<std::size_t... Ints>
-lib::index_sequence<Ints...> downcast(lib::index_sequence<Ints...> seq) {
-  return seq;
-}
-
-template<typename, std::size_t I, typename, typename>
-struct remove_helper;
-
-template<std::size_t I, typename Output>
-struct remove_helper<void, I, lib::index_sequence<>, Output> {
-  using type = Output;
-};
-
-template<std::size_t J, std::size_t... Output, std::size_t I, std::size_t... Input>
-struct remove_helper<lib::enable_if_t<I == J>, I, lib::index_sequence<J, Input...>, lib::index_sequence<Output...>>
-    : remove_helper<void, I, lib::index_sequence<Input...>, lib::index_sequence<Output...>> {};
-
-template<std::size_t J, std::size_t... Output, std::size_t I, std::size_t... Input>
-struct remove_helper<lib::enable_if_t<I != J>, I, lib::index_sequence<J, Input...>, lib::index_sequence<Output...>>
-    : remove_helper<void, I, lib::index_sequence<Input...>, lib::index_sequence<Output..., J>> {};
-} // namespace detail
-
-template<std::size_t I, typename T>
-using remove = typename detail::remove_helper<void, I, decltype(detail::downcast(T())), lib::index_sequence<>>::type;
-
-template<std::size_t... Ints>
-constexpr std::size_t sequence_size(lib::index_sequence<Ints...>) {
-  return sizeof...(Ints);
-}
-
 }} // namespace halcheck::lib
 
 #endif
