d2ec9038938f2221bd84e2536fc4d7545eeb3dcf
YellPika <yellpika@gmail.com>
2024-09-09 11:34:21 -0400

    feat: variant.hpp exports <variant>

--- include/halcheck/lib/variant.hpp
+++ include/halcheck/lib/variant.hpp
@@ -2,7 +2,7 @@
 #define HALCHECK_LIB_VARIANT_HPP
 
 #if __cplusplus >= 201606L
-#include <variant>
+#include <variant> // IWYU pragma: export
 #define HALCHECK_VARIANT_NS ::std
 #else
 #include <mpark/in_place.hpp> // IWYU pragma: export
