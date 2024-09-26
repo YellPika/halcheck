ef703580323226d8b5a31f9381525c118dd9d036
YellPika <yellpika@gmail.com>
2024-09-26 13:08:02 -0400

    refactor(gen): remove unused label overload

--- include/halcheck/gen/label.hpp
+++ include/halcheck/gen/label.hpp
@@ -8,9 +8,6 @@
 #include <halcheck/lib/scope.hpp>
 #include <halcheck/lib/type_traits.hpp>
 
-#include <cstddef>
-#include <initializer_list>
-#include <string>
 #include <utility>
 
 namespace halcheck { namespace gen {
@@ -20,21 +17,13 @@ struct label_effect {
   lib::destructable fallback() const { return nullptr; }
 };
 
-static struct label_t {
+static struct {
   lib::destructable operator()(lib::atom value) const { return eff::invoke<label_effect>(value); }
 
   template<typename F, typename... Args, HALCHECK_REQUIRE(lib::is_invocable<F, Args...>())>
   lib::invoke_result_t<F, Args...> operator()(lib::atom value, F func, Args &&...args) const {
     auto _ = eff::invoke<label_effect>(value);
     return lib::invoke(func, std::forward<Args>(args)...);
-  }
-
-  template<
-      typename... Args,
-      HALCHECK_REQUIRE(lib::is_invocable<label_t, Args...>()),
-      HALCHECK_REQUIRE(sizeof...(Args) > 0)>
-  lib::invoke_result_t<label_t, Args...> operator()(lib::atom value, Args &&...args) const {
-    return (*this)(value, (*this), std::forward<Args>(args)...);
   }
 } label;
 
