945e662243204082e80f7c9033a8af9c156fb31b
YellPika <yellpika@gmail.com>
2024-11-08 16:14:24 -0500

    feat(gen): lib::dag generator

--- include/halcheck/gen/dag.hpp
+++ include/halcheck/gen/dag.hpp
@@ -1,63 +1,75 @@
 #ifndef HALCHECK_GEN_DAG_HPP
 #define HALCHECK_GEN_DAG_HPP
 
-// #include <halcheck/gen/container.hpp>
-// #include <halcheck/gen/label.hpp>
-// #include <halcheck/gen/range.hpp>
-// #include <halcheck/gen/sample.hpp>
-// #include <halcheck/gen/shrink.hpp>
-// #include <halcheck/gen/size.hpp>
-// #include <halcheck/lib/dag.hpp>
-// #include <halcheck/lib/functional.hpp>
-// #include <halcheck/lib/iterator.hpp>
-// #include <halcheck/lib/type_traits.hpp>
+/**
+ * @defgroup gen-dag gen/dag
+ * @brief Generators for lib::dag.
+ * @ingroup gen
+ */
+#include <halcheck/gen/container.hpp>
+#include <halcheck/gen/label.hpp>
+#include <halcheck/lib/atom.hpp>
+#include <halcheck/lib/dag.hpp>
+#include <halcheck/lib/functional.hpp>
+#include <halcheck/lib/iterator.hpp>
+#include <halcheck/lib/type_traits.hpp>
 
-// #include <cstdint>
-// #include <tuple>
-// #include <unordered_map>
-// #include <vector>
+#include <unordered_map>
+#include <vector>
 
 namespace halcheck { namespace gen {
 
-// static const struct dag_t : gen::labelable<dag_t> {
-//   using gen::labelable<dag_t>::operator();
+/**
+ * @brief Generates a random lib::dag.
+ * @tparam T The type of resource identifiers.
+ * @tparam F The type of function used to generate labels.
+ * @param id A unique identifier for the generated value.
+ * @param func A function used to generate labels.
+ * @return A random lib::dag whose labels are generated via @p func.
+ * @details This function participates in overload resolution only if the following conditions hold:
+ * 1. `lib::is_hashable<T>()` holds.
+ * 2. `lib::is_equality_comparable<T>()` holds.
+ * 3. `lib::is_invocable<F, lib::atom, lib::function_view<void(const T &)>>()` holds.
+ *
+ * The function @p func is passed a function `use`, which is used to indicate which nodes should have edges to the node
+ * being generated. Formally, let @f$N(i)@f$ be the set of values passed to `use` during the generation of the @f$i@f$th
+ * node. Node @f$i@f$ is connected (possibly indirectly) to node @f$j > i@f$ if @f$N(i) \cap N(j) \ne \emptyset@f$.
+ * @ingroup gen-dag
+ */
+template<
+    typename T,
+    typename F,
+    HALCHECK_REQUIRE(lib::is_hashable<T>()),
+    HALCHECK_REQUIRE(lib::is_equality_comparable<T>()),
+    HALCHECK_REQUIRE(lib::is_invocable<F, lib::atom, lib::function_view<void(const T &)>>())>
+lib::dag<lib::invoke_result_t<F, lib::atom, lib::function_view<void(const T &)>>> dag(lib::atom id, F func) {
+  using namespace lib::literals;
+  using dag = lib::dag<lib::invoke_result_t<F, lib::atom, lib::function_view<void(const T &)>>>;
+  using map = std::unordered_map<T, lib::iterator_t<dag>>;
 
-//   template<
-//       typename F,
-//       HALCHECK_REQUIRE(lib::is_invocable<F>()),
-//       HALCHECK_REQUIRE(lib::is_tuple_like<lib::invoke_result_t<F>>()),
-//       HALCHECK_REQUIRE(std::tuple_size<lib::invoke_result_t<F>>() == 2),
-//       HALCHECK_REQUIRE(lib::is_forward_range<lib::tuple_element_t<0, lib::invoke_result_t<F>>>()),
-//       HALCHECK_REQUIRE(lib::is_hashable<lib::range_value_t<lib::tuple_element_t<0, lib::invoke_result_t<F>>>>())>
-//   lib::dag<lib::tuple_element_t<1, lib::invoke_result_t<F>>> operator()(F func) const {
-//     using namespace lib::literals;
+  auto _ = gen::label(id);
 
-//     using key_t = lib::range_value_t<lib::tuple_element_t<0, lib::invoke_result_t<F>>>;
-//     using value_t = lib::tuple_element_t<1, lib::invoke_result_t<F>>;
+  map state;
+  dag output;
 
-//     lib::dag<value_t> output;
-//     std::unordered_map<key_t, lib::iterator_t<lib::dag<value_t>>> iterators;
+  for (auto _ : gen::repeat("labels"_s)) {
+    std::vector<T> keys;
+    std::vector<lib::iterator_t<dag>> parents;
 
-//     gen::repeat([&] {
-//       auto pair = lib::invoke(func);
-//       auto &keys = std::get<0>(pair);
-//       auto &value = std::get<1>(pair);
+    auto value = lib::invoke(func, "func"_s, [&](T key) {
+      auto it = state.find(key);
+      if (it != state.end())
+        parents.push_back(it->second);
+      keys.push_back(std::move(key));
+    });
 
-//       std::vector<lib::iterator_t<lib::dag<value_t>>> parents;
-//       for (auto key : keys) {
-//         auto it = iterators.find(key);
-//         if (it != iterators.end())
-//           parents.push_back(it->second);
-//       }
+    auto it = output.emplace(std::move(parents), std::move(value));
+    for (auto &&key : keys)
+      state[key] = it;
+  }
 
-//       auto it = output.emplace(parents, std::move(value));
-//       for (auto key : keys)
-//         iterators[key] = it;
-//     });
-
-//     return output;
-//   }
-// } dag;
+  return output;
+}
 
 }} // namespace halcheck::gen
 
