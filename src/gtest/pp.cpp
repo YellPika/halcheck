a4c76fa3f185126c1843cc430aefc69b591529ce
YellPika <yellpika@gmail.com>
2024-12-17 14:23:28 -0500

    feat(gtest): use GTest's seed by default

--- src/gtest/pp.cpp
+++ src/gtest/pp.cpp
@@ -1,5 +1,6 @@
 #include "halcheck/gtest/pp.hpp"
 
+#include <halcheck/test/config.hpp>
 #include <halcheck/test/deserialize.hpp>
 #include <halcheck/test/random.hpp>
 #include <halcheck/test/serialize.hpp>
@@ -8,6 +9,7 @@
 
 #include "gtest/gtest.h"
 
+#include <random>
 #include <string>
 
 using namespace halcheck;
@@ -16,8 +18,10 @@ test::strategy gtest::default_strategy_effect::fallback() const {
   auto test = ::testing::UnitTest::GetInstance();
   auto info = test->current_test_info();
   auto name = info->test_suite_name() + std::string(".") + info->name();
-  return (test::deserialize(name)  // Run saved test cases first
-          & test::serialize(name)) // Then save new test cases
-         | test::random()          // Produce test cases randomly
-         | test::shrink();         // Shrink failing test cases
+  auto seed = std::mt19937_64(test->random_seed());
+  return test::config(test::set("SEED", seed, true)) | // Use GTest's seed by default
+         (test::deserialize(name)                      // Run saved test cases first
+          & test::serialize(name))                     // Then save new test cases
+         | test::random()                              // Produce test cases randomly
+         | test::shrink();                             // Shrink failing test cases
 }
--- test/main.cpp
+++ test/main.cpp
@@ -15,16 +15,7 @@ int main(int argc, char *argv[]) {
 
   struct handler : lib::effect::handler<handler, gtest::default_strategy_effect> {
     test::strategy operator()(gtest::default_strategy_effect) override {
-      auto test = ::testing::UnitTest::GetInstance();
-      auto info = test->current_test_info();
-      auto name = info->test_suite_name() + std::string(".") + info->name();
-      auto seed = std::mt19937_64(test->random_seed());
-      return test::config(test::set("SEED", seed, true)) | // Use GTest's seed by default
-             glog::filter(                                 // Only show output from the last failing test case
-                 (test::deserialize(name)                  // Run saved test cases first
-                  & test::serialize(name))                 // Then save new test cases
-                 | test::random()                          // Produce test cases randomly
-                 | test::shrink());                        // Shrink failing test cases
+      return glog::filter(gtest::default_strategy());
     }
   };
 
